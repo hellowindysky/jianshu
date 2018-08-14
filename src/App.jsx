33d80a09255b9d643691a1922ca703b9ae0271d6
zhangquan <quanys09@qq.com>
2018-08-14 16:03:20 +0800

    header组件

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
+import Jheader from './components/common/jheader/Jheader';
 
 class App extends Component {
   render() {
     return (
       <div>
-        hello jianshu
+        <Jheader></Jheader>
       </div>
     );
   }
---
+++ src/components/common/jheader/Jheader.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import {HeaderWrapper} from './style';
+
+class Jheader extends Component {
+  render() {
+    return (
+      <HeaderWrapper>
+        header 组件
+      </HeaderWrapper>
+    )
+  }
+}
+
+export default Jheader;
